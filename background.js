2bc1714e6884a33c1a459fe2419c91bd535bf8f4
EinLinuus <github@linusbenkner.de>
2025-02-02 15:49:59 +0100

    basic alt+tab+tab functionality

--- background.js
+++ background.js
@@ -1,23 +1,63 @@
-let lastTabId = null;
-let currentTabId = null;
+let recentTabs = [];
+let tabSwitchDelay = 200;
+let tabSwitchTimeout = null;
+let tabSwitchCount = 0;
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
-  if (currentTabId !== activeInfo.tabId) {
-    lastTabId = currentTabId;
-    currentTabId = activeInfo.tabId;
-  }
+	console.log("onActivated", activeInfo, recentTabs);
+	if(tabSwitchTimeout) {
+		return;
+	}
+
+	if(recentTabs.length === 0) {
+		recentTabs.push(activeInfo.tabId);
+		return;
+	}
+
+	if(recentTabs[recentTabs.length - 1] === activeInfo.tabId) {
+		return;
+	}
+
+	// remove the tab from the recentTabs array
+	const index = recentTabs.indexOf(activeInfo.tabId);
+	if(index !== -1) {
+		recentTabs.splice(index, 1);
+	}
+
+	// add the tab to the end of the recentTabs array
+	recentTabs.push(activeInfo.tabId);
 });
 
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "switch_recent_tab") {
-    switchToLastTab();
-  }
+	if (command === "switch_recent_tab") {
+		switchToLastTab();
+	}
 });
 
 function switchToLastTab() {
-  if (lastTabId) {
-    chrome.tabs.update(lastTabId, { active: true });
-  } else {
-	  alert("No recent tab found");
-  }
+	console.log("switching...", recentTabs);
+	if(tabSwitchTimeout) {
+		clearTimeout(tabSwitchTimeout);
+	}
+
+	tabSwitchCount++;
+
+	setTimeout(() => {
+		tabSwitchTimeout = null;
+
+		console.log("Running action for tabSwitchCount", tabSwitchCount);
+		if(recentTabs.length >= 1 + tabSwitchCount) {
+			chrome.tabs.update(recentTabs[recentTabs.length - 1 - tabSwitchCount], { active: true });
+		}
+
+		// Reset
+		tabSwitchCount = 0;
+	}, tabSwitchDelay);
+
+	return;
+
+	// old
+	if(recentTabs.length >= 2) {
+		chrome.tabs.update(recentTabs[recentTabs.length - 2], { active: true });
+	}
 }
